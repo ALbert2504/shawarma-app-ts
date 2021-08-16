8d6248628511291ad11de1d1c52df43752919b31
Albert Arakelyan <albertarakelyan1998@gmail.com>
2021-08-16 23:33:43 +0400

    fixed responsive, header is available only for users

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import CreateItem from "./pages/CreateItem";
 import ShawarmaList from './pages/ShawarmaList';
 import Auth from "./pages/Auth";
 import Header from "./components/shared/Header";
+import {useSelector} from "react-redux";
+import {RootState} from "./store/rootReducer";
 
 function App() {
+  const {user = null} = useSelector((state: RootState) => state.auth);
+
+
   return (
     <div className="App">
-      <Header />
+      {user && <Header/>}
       <main className="py-4">
         <Route path="/auth" component={Auth} />
         <Route path="/" exact component={CreateItem} />
--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -6,16 +6,19 @@ import {navLinks} from "./Header.config";
 
 const Header: FC = () => {
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar bg="primary" variant="dark" expand="lg">
       <Container>
         <Link to="/" className="navbar-brand">Շաուրմա Չորեքշաբթի</Link>
-        <Nav className="ml-auto">
-          {navLinks.map((navLink: navLinkType): JSX.Element => {
-            return (
-              <Link className="nav-link" to={navLink.link} key={navLink.id}>{navLink.label}</Link>
-            )
-          })}
-        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {navLinks.map((navLink: navLinkType): JSX.Element => {
+              return (
+                <Link className="nav-link" to={navLink.link} key={navLink.id}>{navLink.label}</Link>
+              )
+            })}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
